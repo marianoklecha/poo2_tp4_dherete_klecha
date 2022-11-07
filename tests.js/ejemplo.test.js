e2e583054d9cb95a8d9341e15813e159fd4fe63e
marianoklecha <marianoklecha01@gmail.com>
2022-11-07 18:47:13 -0300

    'test'

--- tests.js/ejemplo.test.js
+++ tests.js/ejemplo.test.js
@@ -3,5 +3,5 @@ const Local = require("../src/Local");
 test("Generar paquete en local", () =>  {
     localInicial = new Local();
     localInicial.generarPaquete();
-    expect(localInicial.paquetesEnCola).toBe(1); 
+    expect(localInicial.paquetesEnCola.length).toBe(1); 
 }) 
