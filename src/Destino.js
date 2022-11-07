8ede4c71db9bc7572c8530eadf4942a9f0b9cc90
marianoklecha <marianoklecha01@gmail.com>
2022-11-07 19:43:14 -0300

    'test'

---
+++ src/Destino.js
@@ -0,0 +1,9 @@
+function Destino(){
+    this.paquetesEnCola = new Array();
+
+    this.recibirPaquete = function(nuevoPaquete){
+        this.paquetesEnCola.push(nuevoPaquete);
+    }
+}
+
+module.exports = Destino;
