81ae5464f62954ce8564820efa8d165479706597
marianoklecha <marianoklecha01@gmail.com>
2022-11-07 19:15:39 -0300

    'test'

--- tests.js/ejemplo.test.js
+++ tests.js/ejemplo.test.js
@@ -4,4 +4,15 @@ test("Generar paquete en local", () =>  {
     localInicial = new Local();
     localInicial.generarPaquete();
     expect(localInicial.paquetesEnCola.length).toBe(1); 
-}) 
+})
+
+test("Pasar paquete de local a centro de facturación", () =>  {
+    localInicial = new Local();
+    localInicial.generarPaquete();
+
+    centroFacturacion = new CentroFacturacion();
+    localInicial.pasarPaqueteA(centroFacturacion);
+    
+    expect(localInicial.paquetesEnCola.length).toBe(0); 
+    expect(centroFacturacion.paquetesEnCola.length).toBe(1); 
+})
