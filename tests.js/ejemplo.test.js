57b3bda4b09f04c1d080da5eb8ec051ecfc811b0
marianoklecha <marianoklecha01@gmail.com>
2022-11-07 19:37:25 -0300

    'test'

--- tests.js/ejemplo.test.js
+++ tests.js/ejemplo.test.js
@@ -32,3 +32,20 @@ test("Pasar paquete de centro de facturación a centro de calidad", () =>  {
     expect(centroFacturacion.paquetesEnCola.length).toBe(0);
     expect(centroCalidad.paquetesEnCola.length).toBe(1); 
 })
+
+test("Pasar paquete de centro de calidad a centro de distribución", () =>  {
+    localInicial = new Local();
+    localInicial.generarPaquete();
+
+    centroFacturacion = new CentroFacturacion();
+    localInicial.pasarPaqueteA(centroFacturacion);
+
+    centroCalidad = new CentroCalidad();
+    centroFacturacion.pasarPaqueteA(centroCalidad);
+
+    centroDistribucion = new CentroDistribucion();
+    centroCalidad.pasarPaqueteA(centroDistribucion);
+
+    expect(centroCalidad.paquetesEnCola.length).toBe(0);
+    expect(centroDistribucion.paquetesEnCola.length).toBe(1); 
+})
