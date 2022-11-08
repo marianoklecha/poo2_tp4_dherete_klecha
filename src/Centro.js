ad4239315d1d53cf7556dc2f080996f3f0165052
marianoklecha <marianoklecha01@gmail.com>
2022-11-08 11:32:18 -0300

    'test'

--- src/Centro.js
+++ src/Centro.js
@@ -9,12 +9,8 @@ function Centro() {
     Centro.prototype.recibirPaquete = function(nuevoPaquete){
         this.paquetesEnCola.push(nuevoPaquete);
     }
-/*
-    this.recibirPaquete = function(nuevoPaquete){
-        this.paquetesEnCola.push(nuevoPaquete);
-    }*/
 
-    this.pasarPaqueteA = function(siguiente){
+    Centro.prototype.pasarPaqueteA = function(siguiente){
         paqueteAPasar = this.paquetesEnCola.pop();
         siguiente.recibirPaquete(paqueteAPasar);
     }
