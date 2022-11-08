8250e31871a079c5d2d571d3ce1c95cb268dee64
marianoklecha <marianoklecha01@gmail.com>
2022-11-08 12:40:48 -0300

    'test'

--- src/Paquete.js
+++ src/Paquete.js
@@ -3,7 +3,7 @@ const Producto = require('./Producto.js');
 function Paquete(){
     this.productos = new Array();
     
-    this.productos.push(new Producto(Tijera,5));
+    this.productos.push(new Producto("Tijera",5));
 }
 
 module.exports = Paquete;
