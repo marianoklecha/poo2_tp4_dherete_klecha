2cf8ec7d6b6465c6cdeb8cc11dee3651c4a271ca
marianoklecha <marianoklecha01@gmail.com>
2022-11-08 12:23:06 -0300

    'test'

--- src/Tiempo.js
+++ src/Tiempo.js
@@ -3,6 +3,12 @@ function Tiempo(){
 
     this.moverPaquete = function(origen,destino){
         origen.pasarPaqueteA(destino);
+    }
+
+    this.moverPaquetes = function(origen,destino){
+        while(origen.paquetesEnCola.length != 0){
+            origen.pasarPaqueteA(destino);
+        }
         this.tiempoActual++;
     }
 }
