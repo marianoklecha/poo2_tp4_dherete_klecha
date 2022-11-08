651610aeb2b653d6f164227f764b337eda55a98a
marianoklecha <marianoklecha01@gmail.com>
2022-11-08 11:51:15 -0300

    'test'

--- src/Tiempo.js
+++ src/Tiempo.js
@@ -1,5 +1,10 @@
 function Tiempo(){
     this.tiempoActual = 0;
+
+    this.moverPaquete = function(origen,destino){
+        origen.pasarPaqueteA(destino);
+        this.tiempoActual++;
+    }
 }
 
 module.exports = Tiempo;
