894988f5f2b722a9b5fbc90f71834fb2f4b613f1
aylendherete <114444320+aylendherete@users.noreply.github.com>
2022-11-10 15:13:25 -0300

    'test'

--- tests.js/limitacionesDeCentroYPaquete.test.js
+++ tests.js/limitacionesDeCentroYPaquete.test.js
@@ -8,4 +8,6 @@ test("Que centro de facturacion tenga como minimo 3 paquetes",()=>{
     expect(()=>{centroFacturacion.recibirPaquete(paquetes)}).toThrow(new Error("El centro supera el maximo o no llega al minimo de paquetes"));
     var paquetes= paquetes[0];
     expect(()=>{centroFacturacion.recibirPaquete(paquetes)}).toThrow(new Error("El centro supera el maximo o no llega al minimo de paquetes"));
+    paquetes=[new Paquete(),new Paquete(),new Paquete(),new Paquete(),new Paquete(),new Paquete(),new Paquete(),new Paquete()];
+    expect(()=>{centroFacturacion.recibirPaquete(paquetes)}).toThrow(new Error("El centro supera el maximo o no llega al minimo de paquetes"));
 })
