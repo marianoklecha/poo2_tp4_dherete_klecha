454a53d60290630c4590ac03855c5b6035850f30
aylendherete <114444320+aylendherete@users.noreply.github.com>
2022-11-10 12:50:47 -0300

    'test'

--- src/LineaDeProduccion.js
+++ src/LineaDeProduccion.js
@@ -1,5 +1,8 @@
 function LineaDeProduccion(...estaciones){
-    var circuito= new Array(estaciones);
+    var circuito= new Array();
+    for(estacion in estaciones){
+        circuito.push(estacion);
+    }
 
 }
 module.exports=LineaDeProduccion;
