fd0600d3540babb07a271f693562780666984e36
aylendherete <114444320+aylendherete@users.noreply.github.com>
2022-11-10 12:27:52 -0300

    'test'

--- tests.js/atributosPaquetes.test.js
+++ tests.js/atributosPaquetes.test.js
@@ -3,8 +3,7 @@ const Destino = require("../src/Destino");
 const CentroFacturacion = require("../src/CentroFacturacion");
 const CentroCalidad = require("../src/CentroCalidad");
 const CentroDistribucion = require("../src/CentroDistribucion");
-const Tiempo = require("../src/Tiempo");
-const { default: expect } = require("expect");
+
 
 test("Paquete con productos", () =>  {
     localInicial = new Local();
