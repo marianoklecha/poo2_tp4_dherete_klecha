b30e9a8c72142cca172bb6afb68ae117482c2324
aylendherete <114444320+aylendherete@users.noreply.github.com>
2022-11-10 15:16:38 -0300

    'test'

--- tests.js/limitacionesDeCentroYPaquete.test.js
+++ tests.js/limitacionesDeCentroYPaquete.test.js
@@ -1,8 +1,10 @@
 
+const { test } = require("picomatch");
+const CentroCalidad = require("../src/CentroCalidad");
 const CentroFacturacion = require("../src/CentroFacturacion");
 const Paquete = require("../src/Paquete");
 
-test("Que centro de facturacion tenga como minimo 3 paquetes",()=>{
+test("Que centro de facturacion tenga como minimo 3 paquetes y maximo 6 en espera",()=>{
     var centroFacturacion= new CentroFacturacion();
     var paquetes=[new Paquete(),new Paquete()];  
     expect(()=>{centroFacturacion.recibirPaquete(paquetes)}).toThrow(new Error("El centro supera el maximo o no llega al minimo de paquetes"));
@@ -10,4 +12,13 @@ test("Que centro de facturacion tenga como minimo 3 paquetes",()=>{
     expect(()=>{centroFacturacion.recibirPaquete(paquetes)}).toThrow(new Error("El centro supera el maximo o no llega al minimo de paquetes"));
     paquetes=[new Paquete(),new Paquete(),new Paquete(),new Paquete(),new Paquete(),new Paquete(),new Paquete(),new Paquete()];
     expect(()=>{centroFacturacion.recibirPaquete(paquetes)}).toThrow(new Error("El centro supera el maximo o no llega al minimo de paquetes"));
+})
+
+test("Que centro de facturacion tenga como minimo 2 paquetes y maximo 5 en espera",()=>{
+    var centroDeCalidad= new CentroCalidad();
+    var paquete=new Paquete();  
+    expect(()=>{CentroCalidad.recibirPaquete(paquetes)}).toThrow(new Error("El centro supera el maximo o no llega al minimo de paquetes"));
+    var paquetes=[new Paquete(),new Paquete(),new Paquete(),new Paquete(),new Paquete(),new Paquete()];
+    expect(()=>{centroCalidad.recibirPaquete(paquetes)}).toThrow(new Error("El centro supera el maximo o no llega al minimo de paquetes"));
+   
 })
