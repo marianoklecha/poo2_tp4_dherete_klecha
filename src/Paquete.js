6e4584bd762f27f1e47dfa47216d5ad5fb93f78f
aylendherete <114444320+aylendherete@users.noreply.github.com>
2022-11-10 15:44:57 -0300

    'test'

--- src/Paquete.js
+++ src/Paquete.js
@@ -2,7 +2,7 @@ const Producto = require('./Producto.js');
 
 function Paquete(){
     this.productos = new Array(new Producto("Tijera",5));
-    this.urgencia=Math.random() * (4 - 1) + 1;
+    this.urgencia=Math.floor(Math.random() * (4 - 1) + 1);
 }
 
 module.exports = Paquete;
