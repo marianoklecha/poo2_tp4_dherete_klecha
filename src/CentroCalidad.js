feaa24baa449005b5ebfcc7a19d97237bf7632de
aylendherete <114444320+aylendherete@users.noreply.github.com>
2022-11-11 12:05:09 -0300

    'test'

--- src/CentroCalidad.js
+++ src/CentroCalidad.js
@@ -7,12 +7,7 @@ function CentroCalidad(){
     }
     this.paquetesEnCola = new Array();
     this.capacidadMaximaEnEspera=5;
-<<<<<<< HEAD
-
-=======
->>>>>>> a23c880a7ee11553df7d35bdb8b76adf55510ba4
 }
-
 CentroCalidad.prototype = Object.create(Centro.prototype);
 CentroCalidad.prototype.constructor = CentroCalidad;
 
