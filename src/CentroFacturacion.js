d313a2054c7d5b803775b3039465950e75de2e5f
aylendherete <114444320+aylendherete@users.noreply.github.com>
2022-11-11 12:31:58 -0300

    'test'

--- src/CentroFacturacion.js
+++ src/CentroFacturacion.js
@@ -6,7 +6,16 @@ function CentroFacturacion(){
         return new CentroFacturacion();
     }
     this.paquetesEnCola = new Array();
+    this.paquetesProcesados=new Array();
     this.capacidadMaximaEnEspera=Math.floor(Math.random() * (7 - 3) + 3);
+    this.procesar=function(){
+        for(var i =0; i<3;i++){
+            if(this.paquetesEnCola.length!=0){
+                this.paquetesProcesados.push(this.paquetesEnCola[i]);
+            }
+
+        }
+    }
 }
 
 CentroFacturacion.prototype = Object.create(Centro.prototype);
