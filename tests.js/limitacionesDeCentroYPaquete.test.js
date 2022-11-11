1a33375ec73202a76bab678a41a38564b810fe82
aylendherete <114444320+aylendherete@users.noreply.github.com>
2022-11-11 12:11:36 -0300

    'test'

--- tests.js/limitacionesDeCentroYPaquete.test.js
+++ tests.js/limitacionesDeCentroYPaquete.test.js
@@ -10,19 +10,15 @@ test("Que centro de facturacion tenga como maximo entre 3 y 6 en espera",()=>{
     expect(()=>{centroFacturacion.recibirPaquete(paquetes)}).toThrow(new Error("El centro supera el maximo de paquetes"));
 })
 
-test("Que centro de facturacion tenga como minimo 2 paquetes y maximo 5 en espera",()=>{
+test("Que centro de facturacion tenga como maximo entre 2 y 5 en espera",()=>{
     var centroDeCalidad= new CentroCalidad();
-    var paquete=new Paquete();  
-    expect(()=>{centroDeCalidad.recibirPaquete(paquete)}).toThrow(new Error("El centro supera el maximo de paquetes"));
     var paquetes=[new Paquete(),new Paquete(),new Paquete(),new Paquete(),new Paquete(),new Paquete()];
     expect(()=>{centroDeCalidad.recibirPaquete(paquetes)}).toThrow(new Error("El centro supera el maximo de paquetes"));
    
 })
 
-test("Que centro de facturacion tenga como minimo 10 paquetes y maximo 30 en espera",()=>{
+test("Que centro de facturacion tenga como maximo entre 10 y 30 en espera",()=>{
     var centroDeDistribucion= new CentroDistribucion();
-    var paquete=new Paquete();  
-    expect(()=>{centroDeDistribucion.recibirPaquete(paquete)}).toThrow(new Error("El centro supera el maximo de paquetes"));
     var paquetes=[new Paquete(),new Paquete(),new Paquete(),new Paquete(),new Paquete(),new Paquete(),new Paquete(),new Paquete(),new Paquete(),new Paquete(),new Paquete(),new Paquete(),new Paquete(),new Paquete(),new Paquete(),new Paquete(),new Paquete(),new Paquete(),new Paquete(),new Paquete(),new Paquete(),new Paquete(),new Paquete(),new Paquete(),new Paquete(),new Paquete(),new Paquete(),new Paquete(),new Paquete(),new Paquete(),new Paquete(),new Paquete(),new Paquete(),new Paquete(),new Paquete(),new Paquete()];
     expect(()=>{centroDeDistribucion.recibirPaquete(paquetes)}).toThrow(new Error("El centro supera el maximo de paquetes"));
    
