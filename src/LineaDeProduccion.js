70764150b65d3950c4de2ef9c350228a1753306e
marianoklecha <marianoklecha01@gmail.com>
2022-11-12 13:18:01 -0300

    'test'

--- src/LineaDeProduccion.js
+++ src/LineaDeProduccion.js
@@ -1,11 +1,10 @@
-const { toThrowErrorMatchingSnapshot } = require("jest-snapshot");
 
 function LineaDeProduccion(...estaciones){
     this.circuito= estaciones;
+    
     this.recorrerCircuito=function(movedor){
         for(var i = this.circuito.length-1;i>0;i--){ 
-            movedor.moverPaquetes(this.circuito[i-1],this.circuito[i]); 
-
+            movedor.moverPaquetes(this.circuito[i-1],this.circuito[i]);
        }
    }
    this.queLargoTiene = function(){
--- src/Movedor.js
+++ src/Movedor.js
@@ -1,13 +1,11 @@
 const tiempoActual= require("../src/Tiempo");
 function Movedor(){
-
     this.moverPaquetes = function(origen,destino){
         while(origen.paquetesEnCola.length != 0){
             origen.pasarPaqueteA(destino);
         }
         tiempoActual.sumarTiempo();
     }
-
 }
 
 module.exports = Movedor;
