4910faa23c2edb80eaa330f86704fcba46526bcd
aylendherete <114444320+aylendherete@users.noreply.github.com>
2022-11-14 19:50:57 -0300

    'test'

--- tests.js/matrizdeLineaDeProduccion.test.js
+++ tests.js/matrizdeLineaDeProduccion.test.js
@@ -1,5 +1,7 @@
 
 const MapaDeProduccion = require("../src/MapaDeProduccion");
+const Paquete = require("../src/Paquete");
+
 
 test("Que la cantidad de lineas de produccion sean mayor o igual a la cantidad de locales",()=>{
     var mapaDeProduccion= new MapaDeProduccion();
@@ -10,5 +12,5 @@ test("Que la cantidad de lineas de produccion sean mayor o igual a la cantidad d
 
 test("Que cada paquete tenga un destino",()=>{
     var paquete=new Paquete(6);
-    expect(paquete.destino.valor).toBeLessOrEqual(4);
+    expect(paquete.destino).toBeLessOrEqual(4);
 })
