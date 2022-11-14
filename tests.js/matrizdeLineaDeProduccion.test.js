b7b8b082ca9aad3245887a6d8685a8c8d9596485
aylendherete <114444320+aylendherete@users.noreply.github.com>
2022-11-14 19:52:04 -0300

    'test'

--- tests.js/matrizdeLineaDeProduccion.test.js
+++ tests.js/matrizdeLineaDeProduccion.test.js
@@ -12,5 +12,5 @@ test("Que la cantidad de lineas de produccion sean mayor o igual a la cantidad d
 
 test("Que cada paquete tenga un destino",()=>{
     var paquete=new Paquete(6);
-    expect(paquete.destino).toBeLessOrEqual(4);
+    expect(paquete.destino).toBeLessThanOrEqual(4);
 })
