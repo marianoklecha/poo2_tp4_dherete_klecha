eff386adf3c3959cc4993d30087d3baea7e5995d
aylendherete <114444320+aylendherete@users.noreply.github.com>
2022-11-14 19:37:21 -0300

    'test'

--- src/MapaDeProduccion.js
+++ src/MapaDeProduccion.js
@@ -1,3 +1,19 @@
+const Local = require("./Local");
+const CentroCalidad = require("../src/CentroCalidad");
+const CentroDistribucion = require("./CentroDistribucion");
+const CentroFacturacion = require("./CentroFacturacion");
+const Centro = require("./Centro");
+const Destino = require("../src/Destino");
 function MapaDeProduccion(){
-    
+    this.cantidadDeFilasDeProduccion=Math.random() * (6 - 1) + 1;
+
+    this.lineasDeProduccion= new Array();
+    this.crearMapa=function(){
+        for (var i=0;i<4+ 1;i++){
+            this.lineasDeProduccion.push(new LineaDeProduccion(new Local(),new CentroCalidad(), new CentroDistribucion(),new CentroFacturacion(), new CentroCalidad(), new Destino() ));
+        }
+
+    }
 }
+
+module.exports=MapaDeProduccion;
--- tests.js/matrizdeLineaDeProduccion.test.js
+++ tests.js/matrizdeLineaDeProduccion.test.js
@@ -1,10 +1,11 @@
-const CentroCalidad = require("../src/CentroCalidad");
-const CentroDistribucion = require("../src/CentroDistribucion");
-const CentroFacturacion = require("../src/CentroFacturacion");
+
+const MapaDeProduccion = require("../src/MapaDeProduccion");
+
+
 
 test("Que la cantidad de lineas de produccion sean mayor o igual a la cantidad de locales",()=>{
     var mapaDeProduccion= new MapaDeProduccion();
     mapaDeProduccion.crearMapa();
-    expect(mapaDeProduccion.cantidadDeCentros).toBeGreaterThanOrEqual(mapaDeProduccion.filasDeProduccion.length);
+    expect(mapaDeProduccion.centros.length).toBeGreaterThanOrEqual(mapaDeProduccion.cantidadDeFilasDeProduccion.length);
     
 })
