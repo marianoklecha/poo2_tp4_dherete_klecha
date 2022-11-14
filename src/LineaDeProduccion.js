e506f4bcf6cb42a04a914682af116ae74e67abe4
aylendherete <114444320+aylendherete@users.noreply.github.com>
2022-11-14 17:12:57 -0300

    'test'

--- src/LineaDeProduccion.js
+++ src/LineaDeProduccion.js
@@ -4,8 +4,8 @@ function LineaDeProduccion(...estaciones){
     
     this.recorrerCircuito=function(movedor){
         for(var i = this.circuito.length-1;i>0;i--){ 
-            if(this.circuito[i].soyCentro()){
-                this.circuito[i].procesarPaquetesEnEspera();
+            if(this.circuito[i-1].soyCentro()){
+                this.circuito[i-1].procesarPaquetesEnEspera();
             }
             movedor.moverPaquetes(this.circuito[i-1],this.circuito[i]);
        }
