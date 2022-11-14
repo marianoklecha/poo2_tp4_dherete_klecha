92eaf0e4faa7b8f017021747c379c766b65cd3c3
aylendherete <114444320+aylendherete@users.noreply.github.com>
2022-11-14 17:40:40 -0300

    'test'

--- src/Movedor.js
+++ src/Movedor.js
@@ -1,10 +1,22 @@
 const tiempoActual= require("../src/Tiempo");
 function Movedor(){
     this.moverPaquetes = function(origen,destino){
-        while(origen.paquetesProcesados.length != 0 | origen.paquetesEnCola.length!=0){
-            origen.pasarPaqueteA(destino);
+
+        if(origen.soyCentro()){
+            while(origen.paquetesProcesados.length != 0 ){
+                origen.pasarPaqueteA(destino);
+            }
+            tiempoActual.sumarTiempo();
         }
-        tiempoActual.sumarTiempo();
+        else{
+            while(origen.paquetesEnCola.length!=0){
+                origen.pasarPaqueteA(destino);
+            }
+            tiempoActual.sumarTiempo();
+            
+        }
+  
+      
     }
 }
 
