ebd01681fd4d0b02b250ef1185cfb6efdf8f6131
aylendherete <114444320+aylendherete@users.noreply.github.com>
2022-11-14 19:12:41 -0300

    'test'

--- tests.js/matrizdeLineaDeProduccion.test.js
+++ tests.js/matrizdeLineaDeProduccion.test.js
@@ -4,6 +4,6 @@ const CentroFacturacion = require("../src/CentroFacturacion");
 
 test("Que la cantidad de lineas de produccion sean mayor o igual a la cantidad de locales",()=>{
     var mapaDeProduccion= new MapaDeProduccion();
-    expect(mapaDeProduccion.cantidadDeCentros).toBeGreaterThanOrEqual(mapaDeProduccion.lineasDeProduccion.length);
+    expect(mapaDeProduccion.cantidadDeCentros).toBeGreaterThanOrEqual(mapaDeProduccion.filasDeProduccion.length);
     
 })
