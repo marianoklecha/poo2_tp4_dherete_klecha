e57210445fd0d1903855731c42fed3b4faa9af02
aylendherete <114444320+aylendherete@users.noreply.github.com>
2022-11-15 20:49:04 -0300

    'test'

--- tests.js/matrizdeLineaDeProduccion.test.js
+++ tests.js/matrizdeLineaDeProduccion.test.js
@@ -89,6 +89,7 @@ test("Que lleguen TODOS los paquetes a destino con tiempo restando urgencia",()=
         cantRecibidos = mapaDeProduccion.contarCantidadDePaquetes(mapaDeProduccion.lineasDeProduccion[0].circuito.length-1);
     }
 
+    
     for(var i=0;i<mapaDeProduccion.cantidadDeFilasDeProduccion;i++){
         (mapaDeProduccion.lineasDeProduccion[i].circuito[5].paquetesEnCola).forEach(paquete => {
             expect(paquete.urgencia).toBeLessThanOrEqual(7);
