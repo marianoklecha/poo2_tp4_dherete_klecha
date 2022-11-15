0cc801534c6fb991d2bf81ac3633067ee7f1c151
marianoklecha <marianoklecha01@gmail.com>
2022-11-15 18:20:11 -0300

    'test'

--- tests.js/limitacionesDeCentroYPaquete.test.js
+++ tests.js/limitacionesDeCentroYPaquete.test.js
@@ -25,7 +25,7 @@ test("Que centro de facturacion tenga como maximo entre 10 y 30 en espera",()=>{
     var centroDeDistribucion= new CentroDistribucion();
     var paquetes=[new Paquete(),new Paquete(),new Paquete(),new Paquete(),new Paquete(),new Paquete(),new Paquete(),new Paquete(),new Paquete(),new Paquete(),new Paquete(),new Paquete(),new Paquete(),new Paquete(),new Paquete(),new Paquete(),new Paquete(),new Paquete(),new Paquete(),new Paquete(),new Paquete(),new Paquete(),new Paquete(),new Paquete(),new Paquete(),new Paquete(),new Paquete(),new Paquete(),new Paquete(),new Paquete(),new Paquete(),new Paquete(),new Paquete(),new Paquete(),new Paquete(),new Paquete()];
     expect(()=>{centroDeDistribucion.recibirPaquete(paquetes)}).toThrow(new Error("El centro supera el maximo de paquetes"));
-   
+    
 })
 
 */
