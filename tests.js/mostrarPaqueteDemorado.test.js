5b8e5792c3a1f76b7ef46ef3569bcc1a24c2ac03
marianoklecha <marianoklecha01@gmail.com>
2022-11-16 17:52:47 -0300

    'test'

---
+++ tests.js/mostrarPaqueteDemorado.test.js
@@ -0,0 +1,23 @@
+const Local = require("../src/Local");
+const MapaDeProduccion = require("../src/MapaDeProduccion");
+const Movedor = require("../src/Movedor");
+const Paquete = require("../src/Paquete");
+const CentroDistribucion = require("../src/CentroDistribucion");
+const Destino = require("../src/Destino");
+
+test("Que lleguen paquetes y saber si llegó demorado",()=>{
+    var mapaDeProduccion= new MapaDeProduccion();
+    mapaDeProduccion.crearMapa();
+    var cont = 1;
+    while(cont != 0){
+        mapaDeProduccion.llevarPaquetesADestino();
+        cont = 0;
+        for (let index = 0; index < 5; index++) {
+            cont+=mapaDeProduccion.contarCantidadDePaquetes(index);
+        }
+        console.log(cont);
+    }
+    expect(cont).toBe(0);
+    expect(mapaDeProduccion.lineasDeProduccion[0].circuito[5].cantidadPaquetesDemorados).toBeGreaterThanOrEqual(0);
+    expect(mapaDeProduccion.contarCantidadDePaquetes(5)).toBeGreaterThanOrEqual(1);
+});
