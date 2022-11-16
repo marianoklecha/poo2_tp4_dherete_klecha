2dd9ea0a8b650cb34a499d23eacb3acf87deaff9
aylendherete <114444320+aylendherete@users.noreply.github.com>
2022-11-15 21:30:27 -0300

    'test'

--- src/CentroDistribucion.js
+++ src/CentroDistribucion.js
@@ -28,6 +28,7 @@ function CentroDistribucion(){
     
 
     this.unificar=function(){
+        if(this.paquetesProcesados.lenght>=2){
         this.paquetesProcesados.sort(((a, b) => b.destino - a.destino));
         for(var i=this.paquetesProcesados-1;i>1;i--){
             if(this.paquetesProcesados[i].urgencia>this.paquetesProcesados[i-1].urgencia && this.paquetesProcesados[i].destino==this.paquetesProcesados[i-1].destino){
@@ -43,6 +44,7 @@ function CentroDistribucion(){
             }
         }
         this.ordenarPaquetesSegunUrgencia();
+    }
     }
 }
 
