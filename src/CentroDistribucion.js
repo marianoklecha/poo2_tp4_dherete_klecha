03b01867dfaf63372d5f2f2cc7d99f11a256ca6b
aylendherete <114444320+aylendherete@users.noreply.github.com>
2022-11-15 21:28:27 -0300

    'test'

--- src/CentroDistribucion.js
+++ src/CentroDistribucion.js
@@ -1,4 +1,5 @@
 const Centro = require('./Centro.js');
+const Paquete = require('./Paquete.js');
 
 function CentroDistribucion(){
     Centro.call();
@@ -10,6 +11,39 @@ function CentroDistribucion(){
     this.maximoAProcesar = 10;
 
     this.capacidadMaximaEnEspera=Math.floor(Math.random() * (30 - 10) + 10);
+
+   this.procesarPaquetesEnEspera=function(){
+        var cont=0;
+        while(cont<this.maximoAProcesar && this.paquetesEnCola.length!=0){
+            if(this.paquetesEnCola.length!=0){
+                var paquete=this.paquetesEnCola.pop()
+                this.paquetesProcesados.push(paquete);
+            }
+            cont+=1;
+            
+        }
+        this.ordenarPaquetesSegunUrgencia(this.paquetesProcesados);
+        this.unificar();
+    }
+    
+
+    this.unificar=function(){
+        this.paquetesProcesados.sort(((a, b) => b.destino - a.destino));
+        for(var i=this.paquetesProcesados-1;i>1;i--){
+            if(this.paquetesProcesados[i].urgencia>this.paquetesProcesados[i-1].urgencia && this.paquetesProcesados[i].destino==this.paquetesProcesados[i-1].destino){
+                
+                var paqueteUnificado=new Paquete();
+                this.paqueteUnificado.destino= this.paquetesProcesados[i].destino;
+                this.paqueteUnificado.urgencia=this.paquetesProcesados[i-1].urgencia;
+                this.paquetesProcesados.splice(i-1,i);
+                this.paqueteUnificado.productos=this.paquetesProcesados[i].productos.concat(this.paquetesProcesados.[i-1].productos);
+                this.paquetesProcesados.pop(paqueteUnificado);
+
+                this.paquetesProcesados.sort(((a, b) => b.destino - a.destino));
+            }
+        }
+        this.ordenarPaquetesSegunUrgencia();
+    }
 }
 
 
