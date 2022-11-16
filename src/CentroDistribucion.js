4421e8e084457b756938da7d00d898c67aee0e91
aylendherete <114444320+aylendherete@users.noreply.github.com>
2022-11-15 21:34:51 -0300

    'test'

--- src/CentroDistribucion.js
+++ src/CentroDistribucion.js
@@ -30,7 +30,7 @@ function CentroDistribucion(){
     this.unificar=function(){
         if(this.paquetesProcesados.lenght>=2){
         this.paquetesProcesados.sort(((a, b) => b.destino - a.destino));
-        for(var i=this.paquetesProcesados.length-1;i>1;i--){
+        for(var i=this.paquetesProcesados.length-1;i>=1;i--){
             if(this.paquetesProcesados[i].urgencia>this.paquetesProcesados[i-1].urgencia && this.paquetesProcesados[i].destino==this.paquetesProcesados[i-1].destino){
                 
                 var paqueteUnificado=new Paquete();
